bfe37a6cee414b599203bf186de049061704cf86
Ben Blue <dalcowboiz@gmail.com>
2017-10-24 17:25:58 -0400

    fix file bug in root.jsx

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './app';
 import { HashRouter } from 'react-router-dom';
 
 const Root = ({ store }) => (
