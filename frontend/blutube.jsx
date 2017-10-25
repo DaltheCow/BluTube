bf42096b68d38feaf1e5db6ea354c8fc3b78d2c1
Ben Blue <dalcowboiz@gmail.com>
2017-10-25 11:26:00 -0400

    complete bootstrapping

--- frontend/blutube.jsx
+++ frontend/blutube.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { signup, login, logout, verifyUsername } from './actions/session_actions';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
-
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
-  window.verifyUsername = verifyUsername;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
