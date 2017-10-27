00535fdba2ae26aa21ceeb21d85df9ac97111b8d
Ben Blue <dalcowboiz@gmail.com>
2017-10-27 18:35:26 -0400

    add videos reducer

--- frontend/reducers/videos_reducer.js
+++ frontend/reducers/videos_reducer.js
@@ -1,7 +1,18 @@
-
+import { RECEIVE_VIDEOS, RECEIVE_VIDEO, REMOVE_VIDEO } from '../actions/video_actions';
 
 const VideosReducer = (state = {}, action) => {
   switch(action.type) {
+    case RECEIVE_VIDEOS: {
+      return Object.assign({}, state, action.videos);
+    }
+    case RECEIVE_VIDEO: {
+      return Object.assign({}, state, { [action.video.id]: action.video });
+    }
+    case REMOVE_VIDEO: {
+      const newState = Object.assign({}, state);
+      delete newState[action.videoId];
+      return newState;
+    }
     default: return state;
   }
 };
