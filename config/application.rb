b543100478459a18445c6bf0286fbca582222c94
Ben Blue <dalcowboiz@gmail.com>
2017-10-27 09:19:42 -0400

    don't add yml

--- config/application.rb
+++ config/application.rb
@@ -16,3 +16,13 @@ module BluTube
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV["s3_bucket"],
+    :access_key_id => ENV["s3_access_key_id"],
+    :secret_access_key => ENV["s3_secret_access_key"],
+    :s3_region => ENV["s3_region"]
+  }
+}
