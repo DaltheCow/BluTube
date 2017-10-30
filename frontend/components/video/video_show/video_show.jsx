d159fcc0782e120a83a5dea5ba4df263915fe3c5
Ben Blue <dalcowboiz@gmail.com>
2017-10-30 15:33:55 -0400

    more sidebar styling

--- frontend/components/video/video_show/video_show.jsx
+++ frontend/components/video/video_show/video_show.jsx
@@ -31,6 +31,16 @@ class VideoShow extends React.Component {
     );
   }
 
+  duration(time) {
+    const secs = time % 60;
+    const mins = Math.floor(time / 60) % 60;
+    const hrs = Math.floor(time / 3600);
+    const seconds = `:${secs < 10 ? '0' : ''}${secs}`;
+    const hours = `${hrs > 0 ? hrs : ''}${hrs > 0 ? ':' : ''}`;
+    const minutes = `${hrs > 0 && mins < 10 ? '0' : ''}${mins}`;
+    return hours + minutes + seconds;
+  }
+
   handleRedirect(id) {
     this.props.fetchVideo(id);
     this.props.fetchVideos();
@@ -53,13 +63,18 @@ class VideoShow extends React.Component {
               {hasVideos ? this.props.videos.map((video, i) => {
                 return (
                   <li key={i} className="related-vid-index-item">
-                    <button onClick={() => this.handleRedirect(video.id)}>
-                      <div className="related-vid-container"><img className="related-vid-thumbnail" src={video.thumbnailUrl} />
-                      <div className="related-vid-duration">{video.duration}</div>
+                    <button className="related-vid-container" onClick={() => this.handleRedirect(video.id)}>
+                      <div className="related-vid-img">
+                        <img className="related-vid-thumbnail" src={video.thumbnailUrl} />
+                        <div className="related-vid-duration">
+                          {this.duration(video.duration)}
+                        </div>
                       </div>
-                      <div className="related-vid-title">{video.title}</div>
-                      <div className="related-vid-channel">{video.author.username}</div>
-                      <div className="related-vid-viewcount">{this.views(video.viewCount)} views</div>
+                      <div className="related-vid-info">
+                        <div className="related-vid-title">{video.title}</div>
+                        <div className="related-vid-channel">{video.author.username}</div>
+                        <div className="related-vid-viewcount">{this.views(video.viewCount)} views</div>
+                      </div>
                     </button>
                   </li>
                 );
