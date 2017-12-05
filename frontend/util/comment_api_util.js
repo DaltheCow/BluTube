efbd50be35a843e194e1255ac9760bd1c7af0960
Ben Blue <dalcowboiz@gmail.com>
2017-12-05 17:26:51 -0500

    update comment api util

--- frontend/util/comment_api_util.js
+++ frontend/util/comment_api_util.js
@@ -1,26 +1,26 @@
-export const fetchComments = (video_id) => ({
+export const fetchComments = (video_id) => ($.ajax({
   url: `api/videos/${video_id}/comments`,
   method: 'get'
-});
+}));
 
-export const fetchComment = (video_id, comment_id) => ({
+export const fetchComment = (video_id, comment_id) => ($.ajax({
   url: `api/videos/${video_id}/comments/${comment_id}`,
   method: 'get'
-});
+}));
 
-export const createComment = (video_id, comment) => ({
+export const createComment = (video_id, comment) => ($.ajax({
   url: `api/videos/${video_id}/comments`,
   method: 'post',
   data: { comment }
-});
+}));
 
-export const updateComment = (video_id, comment) => ({
+export const updateComment = (video_id, comment) => ($.ajax({
   url: `api/videos/${video_id}/comments`,
   method: 'post',
   data: { comment }
-});
+}));
 
-export const deleteComment = (comment_id) => ({
+export const deleteComment = (comment_id) => ($.ajax({
   url: `api/comments/${comment_id}`,
   method: 'delete'
-});
+}));
