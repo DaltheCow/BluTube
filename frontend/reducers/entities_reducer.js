2a9e3dfb9f6fea00bf8f8d44af08d820ffd9c132
Ben Blue <dalcowboiz@gmail.com>
2018-01-02 16:22:49 -0500

    add subscription reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import VideosReducer from './videos_reducer';
 import UsersReducer from './users_reducer';
 import CommentsReducer from './comments_reducer';
+import SubscriptionReducer from './subscription_reducer'
 
 export default combineReducers({
   videos: VideosReducer,
   users: UsersReducer,
-  comments: CommentsReducer
+  comments: CommentsReducer,
+  subscriptions: SubscriptionReducer,
 });
--- frontend/reducers/subscription_reducer.js
+++ frontend/reducers/subscription_reducer.js
@@ -1,20 +1,27 @@
 import { RECEIVE_SUBS, RECEIVE_SUB, REMOVE_SUB } from '../actions/subscription_actions';
-
+import { RECEIVE_VIDEO } from '../actions/video_actions';
 
 const SubscriptionsReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch(action.type) {
     case RECEIVE_SUBS: {
-      return Object.assign({}, action.subs, state);
+      return action.subs;
     }
     case RECEIVE_SUB: {
-      return Object.assign({}, state, {[action.sub.id]: action.sub});
+      return Object.assign({}, state, { [action.sub.id]: action.sub });
     }
     case REMOVE_SUB: {
       let newState = Object.assign({}, state);
       delete newState[action.sub.id];
       return newState;
+    }
+    case RECEIVE_VIDEO: {
+      if (action.video.sub) {
+        return Object.assign({}, state, { [action.video.sub.id]: action.video.sub });
+      } else {
+        return state;
+      }
     }
     default: return state;
   }
