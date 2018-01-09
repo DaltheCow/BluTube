195bb3d81a1ee5c5802860e015a1d4efcb6c740b
Ben Blue <dalcowboiz@gmail.com>
2018-01-09 00:16:55 -0500

    make app container for sidebar management

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,25 +8,76 @@ import VideoShowContainer from './video/video_show/video_show_container';
 import VideoIndexContainer from './video/video_index/video_index_container';
 import Results from './video/video_search_results';
 
-const App = (props) => (
-  <div>
-    {!['/login', '/signup'].includes(props.location.pathname) ? (
-      <header>
-        <div className="under-nav"></div>
-        <Route path="/" component={ NavBarContainer } />
-      </header>
-    ) : (null)}
-    <Switch>
-      <Route exact path="/videos/:videoId" component={ VideoShowContainer } />
-      <Route exact path="/" component={ VideoIndexContainer } />
-      <Route exact path="/results" component={ Results } />
-      <AuthRoute exact path="/login" component={ SessionFormContainer } />
-      <AuthRoute exact path="/signup" component={ SessionFormContainer } />
-      <ProtectedRoute exact path="/upload" component={ VideoFormContainer } />
-      <ProtectedVideoRoute exact path="/upload/:videoId/edit" component={ VideoFormContainer } />
-      <Route path="/" render={() => <Redirect to="/" />} />
-    </Switch>
-  </div>
-);
+//with overlay
+import SideBar1 from './side_bar/side_bar1';
+//without overlay
+import SideBar2 from './side_bar/side_bar2';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateWindowSize = () =>  this.updateWindowSize();
+    this.state = { isSidebarOverlay: window.innerWidth < 1000 };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateWindowSize);
+  }
+
+  updateWindowSize() {
+
+    //here do the check to see if you need to open the sidebar
+
+    this.setState({isSidebarOverlay: window.innerWidth <= 1000});
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowSize);
+  }
+
+  render() {
+    const { sidebarState, location } = this.props;
+    const { isSidebarOverlay } = this.state;
+    const notSessionPath = !['/login', '/signup'].includes(location.pathname);
+    const notVideoPath = !['/videos/'].includes(location.pathname);
+    const mainContent = (
+      <Switch>
+        <Route exact path="/videos/:videoId" component={ VideoShowContainer } />
+        <Route exact path="/" component={ VideoIndexContainer } />
+        <Route exact path="/results" component={ Results } />
+        <AuthRoute exact path="/login" component={ SessionFormContainer } />
+        <AuthRoute exact path="/signup" component={ SessionFormContainer } />
+        <ProtectedRoute exact path="/upload" component={ VideoFormContainer } />
+        <ProtectedVideoRoute exact path="/upload/:videoId/edit" component={ VideoFormContainer } />
+        <Route path="/" render={() => <Redirect to="/" />} />
+      </Switch>
+    );
+
+
+
+    return (
+      <div>
+        {notSessionPath ? (
+          <header>
+            <div className="under-nav"></div>
+            <Route path="/" component={ NavBarContainer } />
+          </header>
+        ) : (null)}
+
+        <Route path="/videos/:videoId" component={ SideBar1 } />
+        <Route path="/videos/:videoId" render={() => mainContent} />
+
+        {notVideoPath && isSidebarOverlay ? (<SideBar1 />) : (null)}
+        {notVideoPath && isSidebarOverlay ? { mainContent } : (null)}
+        {notVideoPath && !isSidebarOverlay ? (
+          <div>
+            <SideBar2 />
+            { mainContent }
+          </div>
+        ) : (null)}
+      </div>
+    );
+  }
+}
 
 export default withRouter(App);
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './app';
+import AppContainer from './app_container';
 import { HashRouter } from 'react-router-dom';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <AppContainer />
     </HashRouter>
   </Provider>
 );
