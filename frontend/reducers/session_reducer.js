88b958d74b2c664f8b1020b147c38dd54ee07d4f
Ben Blue <dalcowboiz@gmail.com>
2018-01-12 20:18:55 -0500

    add subscription addition/removal in session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_SUB, REMOVE_SUB } from '../actions/subscription_actions';
 
 const defaultState = {
   currentUser: null
@@ -9,6 +10,18 @@ const SessionReducer = (state = defaultState, action) => {
   switch(action.type) {
     case RECEIVE_CURRENT_USER: {
       return Object.assign({}, state, {currentUser: action.currentUser});
+    }
+    case RECEIVE_SUB: {
+      const newState = Object.assign({}, state);
+      newState.currentUser = Object.assign({}, newState.currentUser);
+      newState.currentUser.subIds = newState.currentUser.subIds.concat(action.sub.id);
+      return newState;
+    }
+    case REMOVE_SUB: {
+      const newState = Object.assign({}, state);
+      newState.currentUser = Object.assign({}, newState.currentUser);
+      newState.currentUser.subIds = newState.currentUser.subIds.filter(id => id !== action.sub.id);
+      return newState;
     }
     default: return state;
   }
