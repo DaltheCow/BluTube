f207b8ff684d21d277efc35cf4cbab31d431b1c4
Ben Blue <dalcowboiz@gmail.com>
2018-01-12 20:03:31 -0500

    add users upon retrieval of subscriptions

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,14 @@
-import { RECEIVE_COMMENTS, RECEIVE_COMMENT } from '../actions/comment_actions';
+import { RECEIVE_COMMENTS } from '../actions/comment_actions';
+import { RECEIVE_SUBS } from '../actions/subscription_actions';
 
 const UsersReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch(action.type) {
     case RECEIVE_COMMENTS: {
+      return Object.assign({}, action.users, state);
+    }
+    case RECEIVE_SUBS: {
       return Object.assign({}, action.users, state);
     }
     default: return state;
