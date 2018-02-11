ac54dc9c2ae867994ced648d2ebe7348f0d6dba9
Ben Blue <dalcowboiz@gmail.com>
2018-02-11 01:24:34 -0500

    add channel videos

--- frontend/components/video/channel/channel_content/videos.jsx
+++ frontend/components/video/channel/channel_content/videos.jsx
@@ -1,10 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { views, duration, whenPosted } from '../../../../util/component_util';
+
 
 class Videos extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.videos = [];
+  }
+
+  componentDidMount() {
+    $('html,body').scrollTop(0);
+    const { fetchChannelVideos, channelId, videos } = this.props;
+    fetchChannelVideos(channelId);
+  }
+
+  componentWillReceiveProps(newProps) {
+      const { videos } = newProps;
+      if ((videos.length <= 40 && videos.length > this.videos.length) || (videos.length > 40 && this.videos.length < 40)) {
+        this.videos = videos.slice(0, 40).sort((a,b) => a.createdAtInt - b.createdAtInt);
+        this.forceUpdate();
+      }
+    }
+
   render() {
+    const videos = this.videos;
+
     return (
-      <div>VIDEOS</div>
+      <div className="channel-videos-container">
+        <div className="video-index-container">
+
+          <div className="video-index">
+
+            <ul className="video-index-list">
+
+              {videos.map((video, i) => {
+                return (
+                  <li key={i} className="index-index-item">
+
+                    <Link to={`/videos/${video.id}`} className="index-link">
+
+                      <div className="index-img">
+                        <img className="index-thumbnail" src={video.thumbnailUrl} />
+                        <div className="index-duration">
+                          {duration(video.duration)}
+                        </div>
+                      </div>
+
+                      <div className="index-title">{video.title}</div>
+                      <div className="index-channel">{video.author.username}</div>
+
+                      <div className="index-view-time">
+                        <div className="index-viewcount">{views(video.viewCount)} views</div><span className="before-target"></span>
+                        <div>{whenPosted(video.createdAtInt)}</div>
+                      </div>
+
+                    </Link>
+
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- frontend/components/video/channel/channel_content/videos_container.js
+++ frontend/components/video/channel/channel_content/videos_container.js
@@ -1,16 +1,25 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Videos from './videos';
+import { fetchChannelVideos } from '../../../../actions/video_actions';
+import { mapFilter } from '../../../../util/component_util';
 
 const mapStateToProps = (state, ownProps) => {
-  return {
+  const channelId = ownProps.match.params.userId;
+  const channel = state.entities.users[channelId];
+  const videoIds = channel && channel.videoIds ? channel.videoIds : [];
+  const allVids = state.entities.videos;
+  const videos = mapFilter(videoIds, id => allVids[id], a => a !== undefined);
 
+  return {
+    videos,
+    channelId
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    fetchChannelVideos: (channelId) => dispatch(fetchChannelVideos(channelId)),
   };
 };
 
